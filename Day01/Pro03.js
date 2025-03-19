b3a78e728ccba08b57903b661aa2d858d8e090e8
MrManoj124 <Manorooban@gmail.com>
2025-03-18 23:47:40 -0700

    create a method find the given number is prime number or not

--- Day01/Pro03.js
+++ Day01/Pro03.js
@@ -10,3 +10,30 @@ function sum()
 }
 
 console.log(sum())
+
+function sub(a,b)
+{
+    return a-b
+}
+console.log(sub(18,10))
+
+//write a boolean function to find a given number is prime number or not
+function prime(n)
+{
+    if(n<2)
+    {
+        return false
+    }
+    else{
+        for(i=2; i<n; i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+}
